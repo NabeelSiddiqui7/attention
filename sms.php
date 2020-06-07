3eb1cc55fbe1a39e9a2ddc13182734d43b6590df
NabeelSiddiqui7 <58896851+NabeelSiddiqui7@users.noreply.github.com>
2020-06-07 16:24:15 -0400

    Add files via upload

---
+++ sms.php
@@ -0,0 +1,9 @@
+<?php
+$subject = "";
+$to = 6477864664;
+$from = "Nabeel";
+$message = "hello world";
+$carrier = "txt.freedommobile.ca";
+
+mail('nabeel.sddq@gmail.com', $subject, $message, "From: nabeel.sddq@gmail.com");
+?>
